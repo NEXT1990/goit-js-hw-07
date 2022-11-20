337f7f9245d28a48e7412a34cf6481426f60593f
NEXT1990 <maksim.listunov@gmail.com>
2022-11-21 00:09:08 +0200

    add-task-1

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+const imageCardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageCardsMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link"
+    href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"/>
+    </a>
+    </div>
+    `;
+    })
+    .join("");
+
+  return markup;
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const targetImageEl = event.target.classList.contains("gallery__image");
+
+  if (!targetImageEl) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}">
+    </div>
+`);
+
+  instance.show();
+
+  galleryContainer.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.code === "Escape") {
+        console.log(event.code === "Escape");
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+}
