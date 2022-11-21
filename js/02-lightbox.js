07b6ab67cf6457bcbae725773447dd450a1224bf
NEXT1990 <maksim.listunov@gmail.com>
2022-11-22 00:03:34 +0200

    add-task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imageCardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageCardsMarkup);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item"
+    href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+</a>
+    `;
+    })
+    .join("");
+
+  return markup;
+}
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log();
